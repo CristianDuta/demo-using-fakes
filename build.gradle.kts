b24c6ee2495889d030577e5882940cff3155c9f7
Cristian Duta <cristian.duta90@gmail.com>
2021-02-28 13:51:37 +0100

    Added http4k to the project

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,6 +12,11 @@ repositories {
 }
 
 dependencies {
+    implementation(platform("org.http4k:http4k-bom:4.3.5.4"))
+    implementation("org.http4k:http4k-core")
+    implementation("org.http4k:http4k-cloudnative")
+    implementation("org.http4k:http4k-client-apache")
+
     testImplementation(kotlin("test-junit5"))
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.6.0")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.6.0")
