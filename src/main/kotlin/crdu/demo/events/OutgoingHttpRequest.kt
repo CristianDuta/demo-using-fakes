7d3fbb2b0496f913e5cdcd94bf64431ea048bdf0
Cristian Duta <cristian.duta90@gmail.com>
2021-03-05 13:56:29 +0100

    Incoming and Outgoing events

---
+++ src/main/kotlin/crdu/demo/events/OutgoingHttpRequest.kt
@@ -0,0 +1,12 @@
+package crdu.demo.events
+
+import org.http4k.core.Method
+import org.http4k.core.Uri
+import org.http4k.events.Event
+
+data class OutgoingHttpRequest(
+    val uri: Uri,
+    val method: Method,
+    val latency: Long,
+    val status: Int
+) : Event
