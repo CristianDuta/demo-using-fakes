2df46930de4f7e67fd17cc6d823a3645b2266b07
Cristian Duta <cristian.duta90@gmail.com>
2021-03-13 17:34:06 +0100

    Using Path lens instead of routing path

--- src/test/kotlin/crdu/demo/fakes/FakeJsonPlaceholder.kt
+++ src/test/kotlin/crdu/demo/fakes/FakeJsonPlaceholder.kt
@@ -1,18 +1,18 @@
 package crdu.demo.fakes
 
 import org.http4k.core.*
+import org.http4k.lens.Path
+import org.http4k.lens.int
+import org.http4k.lens.string
 import org.http4k.routing.bind
-import org.http4k.routing.path
 import org.http4k.routing.routes
-import org.http4k.server.SunHttp
-import org.http4k.server.asServer
 import org.http4k.template.HandlebarsTemplates
 import org.http4k.template.ViewModel
 import org.http4k.template.viewModel
 
 data class JsonPlaceholderTemplate(
     val templateName: String,
-    val resourceId: String? = null
+    val resourceId: Int? = null
 ) : ViewModel {
     override fun template(): String = "crdu/demo/fakes/$templateName"
 }
@@ -28,20 +28,20 @@ class FakeJsonPlaceholder : HttpHandler {
 
     private val routes = routes(
         "/{name}" bind Method.GET to {
-            val name = it.path("name")!!
+            val name = Path.string().of("name")(it)
 
             Response(Status.OK).with(viewLens of JsonPlaceholderTemplate(name))
         },
         "/{name}/{id}" bind Method.GET to {
-            val name = it.path("name")!!
-            val id = it.path("id")!!
+            val name = Path.string().of("name")(it)
+            val id = Path.int().of("id")(it)
 
             Response(Status.OK).with(viewLens of JsonPlaceholderTemplate("${name}-id", id))
         },
         "/{name}/{id}/{resource}" bind Method.GET to {
-            val name = it.path("name")!!
-            val id = it.path("id")!!
-            val resource = it.path("resource")!!
+            val name = Path.string().of("name")(it)
+            val id = Path.int().of("id")(it)
+            val resource = Path.string().of("resource")(it)
 
             Response(Status.OK).with(viewLens of JsonPlaceholderTemplate("${name}-id-${resource}", id))
         }
