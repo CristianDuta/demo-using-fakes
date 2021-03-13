cb64c7b40817789b2f3440c7ec77f4035b7e1534
Cristian Duta <cristian.duta90@gmail.com>
2021-03-13 17:29:41 +0100

    Added eventType to logged events

--- src/main/kotlin/crdu/demo/events/EventLogger.kt
+++ src/main/kotlin/crdu/demo/events/EventLogger.kt
@@ -1,5 +1,18 @@
 package crdu.demo.events
 
+import org.http4k.events.EventFilter
 import org.http4k.events.Events
+import org.http4k.events.MetadataEvent
+import org.http4k.events.plus
 
 typealias EventLogger = Events
+
+fun addEventType() = EventFilter { next ->
+    { e ->
+        val event = when (e) {
+            is MetadataEvent -> e.event
+            else -> e
+        }
+        next(e + ("type" to event.javaClass.simpleName))
+    }
+}
