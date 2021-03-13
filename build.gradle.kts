864c909b098c3658a472c8b2751d8359debef731
Cristian Duta <cristian.duta90@gmail.com>
2021-03-13 17:34:41 +0100

    Added http4k handlebars and format-jackson

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,10 +16,13 @@ dependencies {
     implementation("org.http4k:http4k-core")
     implementation("org.http4k:http4k-cloudnative")
     implementation("org.http4k:http4k-client-apache")
+    implementation("org.http4k:http4k-format-jackson")
 
     testImplementation(kotlin("test-junit5"))
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.6.0")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.6.0")
+
+    testImplementation("org.http4k:http4k-template-handlebars")
 }
 
 tasks.test {
